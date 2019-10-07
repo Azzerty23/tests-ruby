63a8bdc9bc790d8741eb52f89c018d694af683ae
Azzerty23 <azzerty23@hotmail.fr>
2019-10-07 21:48:04 +0200

    fix: functional version of 06_pig_latin.rb

--- lib/06_pig_latin.rb
+++ lib/06_pig_latin.rb
@@ -14,9 +14,7 @@ consonants = *("a".."z").to_a.unshift("qu", "sch") - vowels
     for word in words.split
 
     # "counts 'qu' as a single phoneme"
-    # word.map! { |l| l.}
         word = word.split('')
-        print "#{word} \n"
         worder = []
         word.each_with_index do |letter, index|
             if letter == "q" and word[index+1] == "u"
@@ -27,27 +25,24 @@ consonants = *("a".."z").to_a.unshift("qu", "sch") - vowels
                 worder << letter
             end
         end
-        print "#{worder} \n"
-        print "#{worder[0]} \n"
-        print "#{worder[2...worder.length]} \n"
-        print "#{[worder[1...worder.length] + worder[0...1]].join('') + "ay"} \n"
+       
     # translation.map! { |w| w.match(/[[:upper:]]/) ? w.downcase.capitalize : w }
 
     # translates a word beginning with a vowel
         if vowels.include?(worder[0].downcase)
-            translation << worder + "ay"
+            translation << worder.join('') + "ay"
 
     # translates a word beginning with three consonants
         elsif ( (consonants.include?(worder[0].downcase)) and (consonants.include?(worder[1].downcase)) and (consonants.include?(worder[2].downcase)) )
-            translation << worder[3...worder.length] + worder[0...3] + "ay"
+            translation << [worder[3...worder.length] + worder[0...3]].join('') + "ay"
 
     # translates a word beginning with two consonants
         elsif ( (consonants.include?(worder[0].downcase)) and (consonants.include?(worder[1].downcase)) )
-            translation << worder[2...worder.length] + worder[0...2] + "ay"
+            translation << [worder[2...worder.length] + worder[0...2]].join('') + "ay"
 
     # translates a word beginning with a consonant
         else
-            translation << worder[1...worder.length] + worder[0...1] + "ay"
+            translation << [worder[1...worder.length] + worder[0...1]].join('') + "ay"
 
         end
     end
@@ -56,18 +51,10 @@ consonants = *("a".."z").to_a.unshift("qu", "sch") - vowels
     return translation.join(' ')
 end
 
-puts translate("quiet")
-puts translate("banana")
+# puts translate("quiet")
+# puts translate("banana")
 # puts translate("Hello")
 # puts translate("Anacrouse")
 # puts translate("Prout")
 # puts translate("eat pIe")
 # puts translate("three potatoes")
-
-
-# def vowel(word)
-#     %w[a e i o u].include?(word[0].downcase)
-#     return word + "ay"
-# end
-
-# print vowel('Abeille')
